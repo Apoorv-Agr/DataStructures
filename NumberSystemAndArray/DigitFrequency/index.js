4821a9cbb2d3ee54ad396694c2506194ba7e954b
Apoorv-Agr <agrawal.av27@gmail.com>
2022-01-13 01:15:32 +0530

    digit frequency

--- NumberSystemAndArray/DigitFrequency/index.js
+++ NumberSystemAndArray/DigitFrequency/index.js
@@ -1,2 +1,13 @@
-console.log('Hello');
+const getGivenDigitFreq = (num, digit) => {
+    let count = 0;
+    num = Number(num);
+    while (num) {
+        let remainder = num % 10;
+        if (remainder === digit) count++;
+        num = Math.floor(num / 10);
+    }
+    return count;
+}
+
+console.log(getGivenDigitFreq(552544, 1));
 
